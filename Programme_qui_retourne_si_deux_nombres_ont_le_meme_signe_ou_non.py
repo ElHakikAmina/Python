a8d625cbd69375bc43b2283aed13074cccbd1314
ElHakikAmina <amina.el.hakik2@gmail.com>
2023-01-16 01:55:27 +0100

    Python

---
+++ Programme_qui_retourne_si_deux_nombres_ont_le_meme_signe_ou_non.py
@@ -0,0 +1,6 @@
+A = int (input("Entrer la valeur A : "))
+B = int (input("Entrer la valeur B : "))
+if A*B>0:
+    print("A et B ont le meme signe")
+else :
+    print("A et B ont deux signes differents")
