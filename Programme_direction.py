2f0ef64e88ff92c7ee56985d0383560bf71a1823
ElHakikAmina <amina.el.hakik2@gmail.com>
2023-01-18 13:54:29 +0100

    Python

---
+++ Programme_direction.py
@@ -0,0 +1,11 @@
+N = int (input("Donner un nombre"))
+if N ==6:
+    print("à droite")
+elif N == 4:
+    print("à gauche")
+elif N ==2:
+    print("en bas")
+elif N==8:
+    print("en haut")
+else:
+    print("erreur")
